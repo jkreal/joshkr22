7def0a9673839c9ca6bf50bf6f40a8190d33915d
Josh <jkrowden@gmail.com>
2022-10-28 16:31:29 -0600

    another commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./Main.css";
+import "./MainQueries.css";
+
+
+class Main extends Component {
+    state = {  } 
+    render() { 
+        return (<div>main div</div>);
+    }
+}
+ 
+export default Main;
